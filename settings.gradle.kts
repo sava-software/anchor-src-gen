979d71a8c6ad4c08a6ba5fdca78742a7364a6174
jpe7s <jpe7s.salt188@passfwd.com>
2025-12-17 07:29:27 -0600

    Update shared build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,16 +3,30 @@ rootProject.name = "anchor-src-gen"
 pluginManagement {
   repositories {
     gradlePluginPortal()
-    maven {
-      name = "savaGithubPackages"
-      url = uri("https://maven.pkg.github.com/sava-software/sava-build")
-      credentials(PasswordCredentials::class)
+    mavenCentral()
+    val gprUser = providers.gradleProperty("savaGithubPackagesUsername")
+      .orElse(providers.environmentVariable("ORG_GRADLE_PROJECT_savaGithubPackagesUsername"))
+      .orElse(providers.environmentVariable("GITHUB_ACTOR"))
+      .orNull
+    val gprToken = providers.gradleProperty("savaGithubPackagesPassword")
+      .orElse(providers.environmentVariable("ORG_GRADLE_PROJECT_savaGithubPackagesPassword"))
+      .orElse(providers.environmentVariable("GITHUB_TOKEN"))
+      .orNull
+    if (!gprUser.isNullOrBlank() && !gprToken.isNullOrBlank()) {
+      maven {
+        url = uri("https://maven.pkg.github.com/sava-software/sava-build")
+        credentials {
+          username = gprUser
+          password = gprToken
+        }
+      }
     }
+//  includeBuild("../sava-build")
   }
 }
 
 plugins {
-  id("software.sava.build") version "0.3.0"
+  id("software.sava.build") version "21.3.3"
 }
 
 apply(plugin = "software.sava.build.feature-jdk-provisioning")
